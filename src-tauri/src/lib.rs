3109369e81a22e70707b0dfc7fb8b3002a20a18e
oomeow <oomeow@outlook.com>
2024-10-28 02:29:40 +0800

    build: aur build

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -96,14 +96,11 @@ pub fn run() -> Result<()> {
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .setup(|app| {
             #[cfg(target_os = "macos")]
             app.set_activation_policy(tauri::ActivationPolicy::Accessory);
-
-            #[cfg(desktop)]
-            app.handle().plugin(tauri_plugin_global_shortcut::Builder::new().build())?;
-            #[cfg(desktop)]
-            app.handle().plugin(tauri_plugin_updater::Builder::new().build())?;
 
             let app_handle = app.handle();
             log::trace!("init system tray");
