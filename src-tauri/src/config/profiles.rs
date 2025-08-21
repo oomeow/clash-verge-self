364807907b0fa477d17dca96d1d9869a207a265e
oomeow <oomeow@outlook.com>
2025-08-21 16:09:42 +0800

    feat: custom app error

--- src-tauri/src/config/profiles.rs
+++ src-tauri/src/config/profiles.rs
@@ -1,11 +1,12 @@
 use super::{EnableFilter, PrfItem};
 use crate::{
+    any_err,
     config::ProfileType,
     enhance::chain::{ChainItem, ScopeType},
+    error::{AppError, AppResult},
     log_err,
     utils::{dirs, help},
 };
-use anyhow::{Context, Result, anyhow, bail};
 use serde::{Deserialize, Serialize};
 use serde_yaml::Mapping;
 use std::{collections::HashMap, fs, io::Write, path::PathBuf};
@@ -73,12 +74,12 @@ impl IProfiles {
         }
     }
 
-    pub fn save_file(&self) -> Result<()> {
+    pub fn save_file(&self) -> AppResult<()> {
         help::save_yaml(&dirs::profiles_path()?, self, Some("# Profiles Config for Clash Verge"))
     }
 
     /// 只修改 current、global chain
-    pub fn patch_config(&mut self, patch: IProfiles) -> Result<()> {
+    pub fn patch_config(&mut self, patch: IProfiles) -> AppResult<()> {
         if self.items.is_none() {
             self.items = Some(vec![]);
         }
@@ -99,7 +100,7 @@ impl IProfiles {
                 for old_uid in old_chain {
                     let item = self
                         .get_item_mut(&old_uid)
-                        .ok_or(anyhow!("failed to find the profile item \"uid:{old_uid}\""))?;
+                        .ok_or(any_err!("failed to find the profile item \"uid:{old_uid}\""))?;
 
                     item.enable = Some(false);
                 }
@@ -108,7 +109,7 @@ impl IProfiles {
             for new_uid in new_chain.iter() {
                 let item = self
                     .get_item_mut(new_uid)
-                    .ok_or(anyhow!("failed to find the profile item \"uid:{new_uid}\""))?;
+                    .ok_or(any_err!("failed to find the profile item \"uid:{new_uid}\""))?;
                 item.enable = Some(true);
             }
 
@@ -162,7 +163,7 @@ impl IProfiles {
     /// append new item
     /// if the file_data is some
     /// then should save the data to file
-    pub fn append_item(&mut self, mut item: PrfItem) -> Result<bool> {
+    pub fn append_item(&mut self, mut item: PrfItem) -> AppResult<bool> {
         let mut restart_core = false;
         if let Some(uid) = item.uid.clone() {
             // save the file data
@@ -171,16 +172,13 @@ impl IProfiles {
                 && let Some(file) = item.file.as_ref()
             {
                 let path = dirs::app_profiles_dir()?.join(file);
-                fs::File::create(path)
-                    .with_context(|| format!("failed to create file \"{file}\""))?
-                    .write(file_data.as_bytes())
-                    .with_context(|| format!("failed to write to file \"{file}\""))?;
+                fs::File::create(path)?.write_all(file_data.as_bytes())?;
             }
 
             if let Some(parent) = item.parent.as_ref() {
                 let profile = self
                     .get_item_mut(parent)
-                    .ok_or(anyhow!("failed to find the profile item \"uid:{parent}\""))?;
+                    .ok_or(any_err!("failed to find the profile item \"uid:{parent}\""))?;
                 match profile.chain.as_mut() {
                     Some(chain) => chain.push(uid.clone()),
                     None => profile.chain = Some(vec![uid.clone()]),
@@ -202,14 +200,14 @@ impl IProfiles {
             }
             self.save_file()?;
         } else {
-            bail!("the uid should not be null");
+            return Err(AppError::InvalidValue("the uid should not be null".to_string()));
         }
 
         Ok(restart_core)
     }
 
     /// reorder items
-    pub fn reorder(&mut self, active_id: String, over_id: String) -> Result<()> {
+    pub fn reorder(&mut self, active_id: String, over_id: String) -> AppResult<()> {
         let mut items = self.items.take().unwrap_or_default();
         let mut old_index = None;
         let mut new_index = None;
@@ -235,7 +233,7 @@ impl IProfiles {
     }
 
     /// update the item value
-    pub fn patch_item(&mut self, uid: &str, item: PrfItem) -> Result<()> {
+    pub fn patch_item(&mut self, uid: &str, item: PrfItem) -> AppResult<()> {
         let mut items = self.items.take().unwrap_or_default();
 
         for each in items.iter_mut() {
@@ -259,19 +257,21 @@ impl IProfiles {
         }
 
         self.items = Some(items);
-        bail!("failed to find the profile item \"uid:{uid}\"")
+        Err(AppError::PatchConfig(format!(
+            "failed to find the profile item \"uid:{uid}\""
+        )))
     }
 
     /// be used to update the remote item
     /// only patch `updated` `extra` `file_data`
-    pub fn update_item(&mut self, uid: &str, mut item: PrfItem) -> Result<()> {
+    pub fn update_item(&mut self, uid: &str, mut item: PrfItem) -> AppResult<()> {
         if self.items.is_none() {
             self.items = Some(vec![]);
         }
 
         // find the item
         self.get_item(uid)
-            .ok_or(anyhow!("failed to find the profile item \"uid:{uid}\""))?;
+            .ok_or(any_err!("failed to find the profile item \"uid:{uid}\""))?;
 
         if let Some(items) = self.items.as_mut() {
             let some_uid = Some(uid);
@@ -286,16 +286,10 @@ impl IProfiles {
                     if let Some(file_data) = item.file_data.take() {
                         let file = each.file.take();
                         let file = file.unwrap_or(item.file.take().unwrap_or(format!("{uid}.yaml")));
-
                         // the file must exists
                         each.file = Some(file.clone());
-
                         let path = dirs::app_profiles_dir()?.join(&file);
-
-                        fs::File::create(path)
-                            .with_context(|| format!("failed to create file \"{file}\""))?
-                            .write(file_data.as_bytes())
-                            .with_context(|| format!("failed to write to file \"{file}\""))?;
+                        fs::File::create(path)?.write_all(file_data.as_bytes())?;
                     }
 
                     break;
@@ -308,56 +302,77 @@ impl IProfiles {
 
     /// delete item
     /// if delete the current then return true
-    pub fn delete_item(&mut self, uid: String) -> Result<bool> {
+    pub fn delete_item(&mut self, uid: String) -> AppResult<bool> {
         let current = self.current.as_ref().unwrap_or(&uid);
+        let mut restart_core;
 
-        let profile = self
-            .get_item(&uid)
-            .ok_or(anyhow!("failed to find the profile item \"uid:{uid}\""))?;
+        let profile = self.get_item(&uid);
+        if let Some(profile) = profile {
+            // delete profile and profile chains
+            let mut remove_uids = vec![uid.clone()];
+            if let Some(profile_chain) = profile.chain.as_ref() {
+                // 删除的 profile 有 chain 时, 需要一同删除
+                remove_uids.extend(profile_chain.clone());
+                profile_chain
+                    .iter()
+                    .filter_map(|chain_uid| self.get_item(chain_uid))
+                    .for_each(|o| {
+                        tracing::debug!("delete profile chains");
+                        log_err!(o.delete_file())
+                    });
+            }
+            profile.delete_file()?;
 
-        // delete profile and profile chains
-        let mut remove_uids = vec![uid.clone()];
-        if let Some(profile_chain) = profile.chain.as_ref() {
-            remove_uids.extend(profile_chain.clone());
-            profile_chain
-                .iter()
-                .filter_map(|chain_uid| self.get_item(chain_uid))
-                .for_each(|o| {
-                    tracing::debug!("delete profile chains");
-                    log_err!(o.delete_file())
-                });
-        }
-        profile.delete_file()?;
+            // check if need to restart core
+            let delete_current = uid == *current;
+            restart_core = delete_current;
 
-        // check if need to restart core
-        let delete_current = uid == *current;
-        let mut restart_core = delete_current;
-
-        if let Some(parent) = profile.parent.as_ref()
-            && *parent == *current
-            && let Some(enable) = profile.enable.as_ref()
-            && *enable
-        {
-            restart_core = true;
-        }
-
-        if let Some(items) = self.items.as_mut() {
-            items.retain(|i| {
-                if let Some(uid_) = i.uid.as_ref()
-                    && !remove_uids.contains(uid_)
+            let mut parent_profile = None;
+            if let Some(parent) = profile.parent.as_ref() {
+                // 修改父级订阅下的 chain，移除当前删除的 chain uid
+                parent_profile = self.get_item(parent).cloned();
+                if let Some(parent_profile) = parent_profile.as_mut()
+                    && let Some(p_chains) = parent_profile.chain.as_mut()
                 {
-                    true
-                } else {
-                    false
+                    p_chains.retain(|i| i != &uid);
                 }
-            });
-            if delete_current {
-                if let [first, ..] = items.as_slice()
-                    && let Some(uid) = first.uid.as_ref()
+                // 判断是否需要重启内核
+                if *parent == *current
+                    && let Some(enable) = profile.enable.as_ref()
+                    && *enable
                 {
-                    self.current = Some(uid.clone());
-                } else {
-                    self.current = None
+                    restart_core = true;
+                }
+            }
+
+            let items = self.items.as_mut();
+            if let Some(items) = items {
+                // 如果删除的是某个订阅下的 chain，需要修改该订阅下的 chain 数据，移除删除的 chain 的 uid
+                // 该实现比较丑陋，需要重新优化
+                if let Some(parent_profile) = parent_profile
+                    && let Some(index) = items.iter().position(|i| i.uid == parent_profile.uid)
+                {
+                    items.retain(|i| i.uid != parent_profile.uid);
+                    items.insert(index, parent_profile);
+                }
+                // 移除当前删除的 profile 相关联的其他 profile
+                items.retain(|i| {
+                    if let Some(uid_) = i.uid.as_ref()
+                        && !remove_uids.contains(uid_)
+                    {
+                        true
+                    } else {
+                        false
+                    }
+                });
+                if delete_current {
+                    if let [first, ..] = items.as_slice()
+                        && let Some(uid) = first.uid.as_ref()
+                    {
+                        self.current = Some(uid.clone());
+                    } else {
+                        self.current = None
+                    }
                 }
             }
         } else {
