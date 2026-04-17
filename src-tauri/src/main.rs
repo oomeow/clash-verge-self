2b6e98715eb38bba7a182a66c8c02f18d460002a
oomeow <oomeow@outlook.com>
2026-04-17 23:16:17 +0800

    fix(tauri): kill mihomo process during debug mode

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,14 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    #[cfg(debug_assertions)]
+    {
+        // 防止开发调试代码时，tauri 的热更新导致 mihomo 进程不能及时清理
+        let mut system = sysinfo::System::new();
+        system.refresh_all();
+        for process in system.processes_by_name("self-mihomo".as_ref()) {
+            process.kill();
+        }
+    }
     clash_verge_self_lib::run().unwrap();
 }
