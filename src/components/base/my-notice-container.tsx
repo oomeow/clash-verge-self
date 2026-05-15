46c5a610ef55a5d198878a90583f2a26f3cf98cb
oomeow <oomeow@outlook.com>
2026-05-15 13:35:46 +0800

    feat(notice): theme-aware notice container colors

--- src/components/base/my-notice-container.tsx
+++ src/components/base/my-notice-container.tsx
@@ -8,6 +8,7 @@ import IconButton from "@mui/material/IconButton";
 import { CustomContentProps, SnackbarContent, useSnackbar } from "notistack";
 import { ForwardedRef, useCallback } from "react";
 
+import { useThemeModeStore } from "@/stores";
 import { cn } from "@/utils";
 
 import { useCustomTheme } from "../layout/use-custom-theme";
@@ -19,6 +20,7 @@ type MyNoticeContainerProps = CustomContentProps & {
 
 export const MyNoticeContainer = (props: MyNoticeContainerProps) => {
   const { ref, id, variant, message } = props;
+  const themeMode = useThemeModeStore((s) => s.themeMode);
   const { theme } = useCustomTheme();
   const { closeSnackbar } = useSnackbar();
 
@@ -34,6 +36,29 @@ export const MyNoticeContainer = (props: MyNoticeContainerProps) => {
     error: Cancel,
   };
   const Icon = icons[variant];
+  const defaultColor = themeMode === "dark" ? "#4B4B4B" : "#313131";
+  const infoColor = theme.palette.info.main;
+  const warningColor = theme.palette.warning.main;
+  const successColor = theme.palette.success.main;
+  const errorColor = theme.palette.error.main;
+
+  let contentColor;
+  switch (variant) {
+    case "info":
+      contentColor = infoColor;
+      break;
+    case "warning":
+      contentColor = warningColor;
+      break;
+    case "success":
+      contentColor = successColor;
+      break;
+    case "error":
+      contentColor = errorColor;
+      break;
+    default:
+      contentColor = defaultColor;
+  }
 
   return (
     <ThemeProvider theme={theme}>
@@ -41,12 +66,13 @@ export const MyNoticeContainer = (props: MyNoticeContainerProps) => {
         ref={ref}
         className="max-w-125 overflow-hidden rounded-md shadow-xl">
         <div
+          style={{ backgroundColor: contentColor }}
           className={cn("flex w-full items-center p-3", {
-            "bg-[#313131] dark:bg-[#4B4B4B]": variant === "default",
-            "bg-[#43A047] dark:bg-[#16681B]": variant === "success",
-            "bg-[#2196F3] dark:bg-[#0B5E9E]": variant === "info",
-            "bg-[#FF9800] dark:bg-[#A66300]": variant === "warning",
-            "bg-[#D32F2F] dark:bg-[#890F0F]": variant === "error",
+            // "bg-[#313131] dark:bg-[#4B4B4B]": variant === "default",
+            // "bg-[#43A047] dark:bg-[#16681B]": variant === "success",
+            // "bg-[#2196F3] dark:bg-[#0B5E9E]": variant === "info",
+            // "bg-[#FF9800] dark:bg-[#A66300]": variant === "warning",
+            // "bg-[#D32F2F] dark:bg-[#890F0F]": variant === "error",
           })}>
           <div className="flex w-full items-center overflow-hidden">
             {variant !== "default" && <Icon className="fill-white!" />}
