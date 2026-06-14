567e1576fadbd2def6c4613255e84a3f40c7ae45
oomeow <oomeow@outlook.com>
2026-06-14 17:11:08 +0800

    refactor(log-item): improve layout and styling of log item component

--- src/components/log/log-item.tsx
+++ src/components/log/log-item.tsx
@@ -16,21 +16,21 @@ const LogItem = (props: Props) => {
   }
   msg = msg.replaceAll(" error: ", " ⇢ error ");
 
+  const type = value.type.toLowerCase();
+
   return (
-    <div className="hover:bg-primary/8 dark:hover:bg-primary/14 rounded-lg px-3 py-2 select-text">
+    <div className="px-4 py-2">
       <div className="flex items-center gap-2">
         <span
-          className="data-[type=error]:bg-error/12 data-[type=error]:text-error data-[type=err]:bg-error/12 data-[type=err]:text-error data-[type=warning]:bg-warning/12 data-[type=warning]:text-warning data-[type=warn]:bg-warning/12 data-[type=warn]:text-warning data-[type=info]:bg-info/12 data-[type=info]:text-info data-[type=inf]:bg-info/12 data-[type=inf]:text-info data-[type=debug]:bg-text-secondary/10 data-[type=debug]:text-text-secondary inline-block rounded-full px-1.5 py-0.5 text-center text-[10px] leading-normal font-semibold uppercase"
-          data-type={value.type.toLowerCase()}>
+          className="data-[type=debug]:bg-text-secondary/10 data-[type=debug]:text-text-secondary data-[type=error]:bg-error/12 data-[type=error]:text-error data-[type=err]:bg-error/12 data-[type=err]:text-error data-[type=info]:bg-info/12 data-[type=info]:text-info data-[type=inf]:bg-info/12 data-[type=inf]:text-info data-[type=warn]:bg-warning/12 data-[type=warn]:text-warning data-[type=warning]:bg-warning/12 data-[type=warning]:text-warning inline-flex h-5 min-w-11 items-center justify-center rounded-sm px-1.5 text-[11px] leading-none font-bold uppercase"
+          data-type={type}>
           {value.type}
         </span>
-        <span className="text-text-secondary text-xs">{value.time}</span>
+        <span className="text-text-secondary/60 text-[11px]">{value.time}</span>
       </div>
-      <div className="mt-1">
-        <span className="text-text-primary break-anywhere text-sm leading-snug">
-          {msg}
-        </span>
-      </div>
+      <p className="text-text-primary break-anywhere mt-1 text-sm leading-normal select-text">
+        {msg}
+      </p>
     </div>
   );
 };
